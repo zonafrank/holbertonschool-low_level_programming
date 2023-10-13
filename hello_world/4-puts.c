2d1efe18a3e22be7576123804abab75036f8a509
echezona <echyth@yahoo.com>
2023-10-13 09:43:41 -0700

    modify script 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-  char message[] = "Programming is like building a multilingual puzzle\n";
+  char message[] = "\"Programming is like building a multilingual puzzle\n";
   puts(message);
   return 0;
 }
