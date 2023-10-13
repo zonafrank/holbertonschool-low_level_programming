559fe85713c2ff58426fd684f8b497e91827d942
echezona <echyth@yahoo.com>
2023-10-13 09:34:19 -0700

    create script 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+*/
+int main(void)
+{
+  char message[] = "Programming is like building a multilingual puzzle";
+  puts(message);
+  return 0;
+}
