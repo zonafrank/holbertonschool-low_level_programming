369a0b6235e5bc678352460674ead5b5a15e0ee2
echezona <echyth@yahoo.com>
2023-10-13 10:08:21 -0700

    create modify 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	char message[] = "with proper grammar, but the outcome is a piece of art";
+	char message[] = "with proper grammar, but the outcome is a piece of art,";
 
 	printf("%s\n", message);
 	return (0);
