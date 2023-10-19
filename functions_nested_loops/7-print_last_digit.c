b25ffed14c69f782df7d1066139f7f2d5c158277
Eche <echyth@yahoo.com>
2023-10-20 01:23:57 +1000

    modify function 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,8 @@
 
 int print_last_digit(int num)
 {
-	int lastDigit = abs(num) % 10;
+	int absNum = num < 0 ? num * -1 : num;
+	int lastDigit = absNum % 10;
 	_putchar(lastDigit);
 	return (lastDigit);
 }
