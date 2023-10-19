155065c54170afc6c175205468c9fa121ec49ad2
Eche <echyth@yahoo.com>
2023-10-20 01:21:35 +1000

    initial commit of 7-print_last_digit and update of main.h

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit: given a number it prints and returns the last digit
+ * @num: integer for which to find the last digit
+ * Returns: value of the last digit
+ */
+
+int print_last_digit(int num)
+{
+	int lastDigit = abs(num) % 10;
+	_putchar(lastDigit);
+	return (lastDigit);
+}
