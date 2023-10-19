f247a0ae5c048c4064424b67d6a4609bca0104da
Eche <echyth@yahoo.com>
2023-10-20 08:41:26 +1000

    initial commit for 9-times_table.c, update for main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints out the multiplication table from 0 to 9
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			int product = i * j;
+			if (product > 9)
+			{
+				_putchar('0' + (product / 10));
+			}
+			_putchar('0' + (product % 10));
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar('\t')
+			}
+		}
+		printf("\n");
+	}
+}
