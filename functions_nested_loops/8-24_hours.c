935faad080ada036bce0ce010f631e0b88edabc1
Eche <echyth@yahoo.com>
2023-10-20 07:43:16 +1000

    8-24_hours.c:update code logic

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,5 +6,25 @@
 
 void jack_bauer(void)
 {
+	int secondsInDay = 24 * 60 * 60;
+	int secondsInHour = 60 * 60;
+	int secondsInMinutes = 60;
+	int count = 0;
+
+	while (count < secondsInDay)
+	{
+		int seconds = count;
+		int hours;
+		int minutes;
+
+		hours = seconds / secondsInHour;
+		seconds = seconds % secondsInHour;
+		minutes = seconds / secondsInMinutes;
+		seconds = seconds % secondsInMinutes;
+
+		printf("%02d:%02d:%02d", hours, minutes, seconds);
+		printf("\n");
+		count++;
+	}
 	return;
 }
