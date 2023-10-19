74aafefe37b7f1d1b244879347c2395d662d80e8
Eche <echyth@yahoo.com>
2023-10-19 10:19:41 +1000

    modify 5-print_numbers.c for coding style checker

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        char ch = '0';
+	char ch = '0';
 
-        while (ch <= '9')
-        {
-                putchar(ch);
-                ch++;
-        }
-        putchar('\n');
-        return (0);
+	while (ch <= '9')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
