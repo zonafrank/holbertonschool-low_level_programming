f038258554c983b32919efec0a08a45597bbaa32
Eche <echyth@yahoo.com>
2023-10-19 10:17:48 +1000

    initial commit of 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        char ch = '0';
+
+        while (ch <= '9')
+        {
+                putchar(ch);
+                ch++;
+        }
+        putchar('\n');
+        return (0);
+}
