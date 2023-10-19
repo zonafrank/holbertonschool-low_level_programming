fcd6ac75b5788f2342b018377fe79c2410d984c2
Eche <echyth@yahoo.com>
2023-10-20 00:18:47 +1000

    initial commit of 4-isalpha and update of main.h

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+/**
+ * _isalpha - checks for alphanumeric character
+ * @c: character to check
+ *
+ * Return: 1 if argument is alphanumeric else returns 0
+ */
+
+int _islower(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
