68a631bf2bb1f86baacd53a3868cc82686d18775
Eche <echyth@yahoo.com>
2023-10-21 21:01:28 +1000

    update 0-isupper.c for test cases

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,9 @@
+/**
+ * _isupper - a function that checks for uppercase character
+ * @ch: character to check for
+ * Return: 1 if ch is uppercase else 0
+ */
+
 int _isupper(char ch)
 {
 	int result = 0;
@@ -6,5 +12,5 @@ int _isupper(char ch)
 	{
 		result = 1;
 	}
-	return result;
+	return (result);
 }
