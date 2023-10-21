1564c3033c360fe63db08f58b5839a0471de14b9
Eche <echyth@yahoo.com>
2023-10-21 23:51:09 +1000

    update 6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line -  draws a straight line in the terminal.
+ * @n: the number of times _ should be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('_');
+	}
+}
