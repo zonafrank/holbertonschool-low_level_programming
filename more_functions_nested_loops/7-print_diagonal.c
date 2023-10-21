c67401e1598f22f594c1ecf943ff7d3968fe0d6f
Eche <echyth@yahoo.com>
2023-10-22 00:17:45 +1000

    update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (int j = 0; j <= i; j++)
+			for (j = 0; j <= i; j++)
 			{
 				if (i == j)
 				{
