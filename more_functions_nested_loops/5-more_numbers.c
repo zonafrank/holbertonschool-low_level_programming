f60bc9ff774e33eb472c78fb36c14f654ba5b0bb
Eche <echyth@yahoo.com>
2023-10-21 23:27:05 +1000

    update main.h and create 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_multi -  helper function for printing out multi digit integers
+ * @value: integer value to be printed out
+ * Return: void
+ */
+void print_multi(long value)
+{
+	if (value != 0)
+	{
+		print_multi(value / 10);
+		putchar((value % 10) + '0');
+	}
+}
+
+/**
+ *more_numbers -  prints 10 times the numbers, from 0 to 14
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (j = 0; j <= 14; j++)
+		{
+			print_multi(j);
+		}
+		putchar('\n');
+	}
+	return;
+}
