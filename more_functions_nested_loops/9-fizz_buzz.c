5719560329c0b926a5fb321736dd06e1f45716c8
Eche <echyth@yahoo.com>
2023-10-22 00:42:20 +1000

    initial commit for 9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+	}
+}
