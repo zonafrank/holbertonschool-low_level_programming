880ea99b0f4cf984bbe8699e233353d543bd92c1
Eche <echyth@yahoo.com>
2023-10-25 00:29:37 +1000

    update 8-print_arrya.c: fix code style errors

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: pointer to array to be printed from
+ * @n: number of values to print from the array
+ *
+ * Return: void
+ */
 void print_array(int *a, int n)
 {
 	int i;
