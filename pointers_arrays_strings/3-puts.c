050aa83c160c87e70805292f744048c5ded235ba
Eche <echyth@yahoo.com>
2023-10-24 22:15:33 +1000

    update main.h and create 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to string literal
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int count = 0;
+	char ch;
+
+	do
+	{
+		ch = str[count];
+		if (ch == '\0')
+			_putchar('\n');
+		else
+			_putchar(ch);
+		count++;
+	} while (ch != '\0');
+}
