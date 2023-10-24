f1e4c96e0d41a951f95a3bd66ec921f39641df9f
Eche <echyth@yahoo.com>
2023-10-24 21:15:41 +1000

    update main.h and create 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first integer
+ * @b: pointer to sedonc integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
