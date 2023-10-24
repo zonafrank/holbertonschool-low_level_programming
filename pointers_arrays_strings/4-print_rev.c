075b640c3c3cac4c78d56476f62fe3f0eedf3c0e
Eche <echyth@yahoo.com>
2023-10-24 22:54:13 +1000

    modify main.h and create 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: pointer to string literal to be printed in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	char ch;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (length >= 0)
+	{
+		ch = s[length];
+		_putchar(ch);
+		if (length == 0)
+		{
+			_putchar('\n');
+		}
+		length--;
+	}
+}
