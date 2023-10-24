e389120b21a814a024125e4233b95efc4ecd7891
Eche <echyth@yahoo.com>
2023-10-24 22:26:23 +1000

    update main.h and create 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -11,16 +11,6 @@ void _puts(char *str)
 	int count = 0;
 	char ch;
 
-	// do
-	// {
-	// 	ch = str[count];
-	// 	if (ch == '\0')
-	// 		_putchar('\n');
-	// 	else
-	// 		_putchar(ch);
-	// 	count++;
-	// } while (ch != '\0');
-
 	while (1)
 	{
 		ch = str[count];
