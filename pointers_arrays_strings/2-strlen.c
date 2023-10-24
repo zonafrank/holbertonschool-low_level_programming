aa4fcace5dae9b4820c6a16398b67f3bac79b7da
Eche <echyth@yahoo.com>
2023-10-24 21:57:46 +1000

    update main.h and implement initial _strlen function

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string literal
+ *
+ * Return: length of the string literal
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	return count;
+}
