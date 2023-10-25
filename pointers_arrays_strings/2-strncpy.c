06e879af1f7f41a96ceba609ee1a8c8805a04dc6
Eche <echyth@yahoo.com>
2023-10-25 21:37:41 +1000

    update main.h and create 2-strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ * _strncpy - a function that copies a string
+ * @dest: pointer where string will be copied to
+ * @src: string to be copied to dest
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return dest;
+}
