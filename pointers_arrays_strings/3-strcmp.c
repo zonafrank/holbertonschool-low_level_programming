a6741e6c8b57a35b1c48318133192a1107fabd08
Eche <echyth@yahoo.com>
2023-10-25 22:19:25 +1000

    modify 3-strcmp.c: fix coding style errors

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * @s1: pointer for the first string
  * @s2: pointer for the second string
  *
- * Return - less than 0 if s1 is less than s2, 0 if they're equal,
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
  * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
