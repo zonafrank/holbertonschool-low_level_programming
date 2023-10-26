b43df81da67aaeeb6362d4f1a516c79d5206fcec
Eche <echyth@yahoo.com>
2023-10-27 07:28:25 +1000

    update main.h and initial commit of 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the start of memory addresses to fill
+ * @b: char value to fill the memory addresses with
+ * @n: number of memory addresses to fill
+ *
+ * Return: pointer to the first address of addresses filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+
+	return s;
+}
