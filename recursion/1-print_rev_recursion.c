b53d515ce6eb101e576cd6c96d8db29e8b4076d4
Eche <echyth@yahoo.com>
2023-10-30 21:58:39 +1000

    update main.h and create 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to first character of string to be reversed
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
