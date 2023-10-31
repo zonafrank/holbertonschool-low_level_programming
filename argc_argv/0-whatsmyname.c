994720062f9ee56d89d5ca465db600d8446cfd50
Eche <echyth@yahoo.com>
2023-11-01 08:21:49 +1000

    add documentation for function main in 0-whaatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - entry point for the program
+ @argc: number of arguments including the name of the file
+ @argv: array of pointers to strings representing command line arguments
+ Return: Always 0 (success)
+*/
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
