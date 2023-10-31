846bbfca18991557a8970ffbfe5abdd26cf658dd
Eche <echyth@yahoo.com>
2023-11-01 08:41:21 +1000

    initial commit of 1-argc.c

--- argc_argv/1-argc.c
+++ argc_argv/1-argc.c
@@ -2,6 +2,7 @@
 
 /**
  *main - entry point for the program
+ *prints the number of command line arguments
  *@argc: number of arguments including the name of the file
  *@argv: array of pointers to strings representing command line arguments
  *Return: Always 0 (success)
