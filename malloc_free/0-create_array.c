f119240c6e56ef232cb13bf9c01614ac2bd700c3
Eche <echyth@yahoo.com>
2023-11-06 23:40:28 +1000

    initial implementation of create_array function

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_Array - creates an array of chars, and initializes it with
+ *  a specific char
+ * @size: size of the array
+ * @c: character to fill the array with
+ * Return: NULL if array could not be created, else pointer to the
+ * first array element
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	int i;
+
+	buffer = malloc(sizeof(char) * size);
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		buffer[i] = c;
+	}
+	buffer[i] = '\0';
+	return (buffer);
+}
