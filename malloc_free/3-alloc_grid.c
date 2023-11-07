e8752a58c4f430193277d6e3bc9740448f57035c
Eche <echyth@yahoo.com>
2023-11-07 23:18:08 +1000

    fix syntax error in 3-alloc_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int **)malloc(width * sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (!grid[i])
 		{
 			for (j = 0; j < i; j++)
