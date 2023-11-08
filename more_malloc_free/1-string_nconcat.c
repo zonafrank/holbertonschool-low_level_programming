ad1a7d3317fbe77c9b4d35f9ce55aca4e20b326b
Eche <echyth@yahoo.com>
2023-11-08 22:53:29 +1000

    update main.h and initial commit of 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s1_size, s2_size, i, j;
+	int s_size;
+	char *s;
+
+	for (s1_size = 0; s1[s1_size] != '\0'; s1_size++)
+		;
+
+	for (s2_size = 0; s2[s2_size] != '\0'; s2_size++)
+		;
+
+	if (n >= s2_size)
+	{
+		s_size = s1_size + s2_size;
+	}
+	else if (n < s2_size)
+	{
+		s_size = s1_size + n;
+	}
+
+	s = malloc((sizeof(char) * s_size) + 1);
+
+	if (!s)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < s1_size)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+
+	if (n < s2_size)
+	{
+		while (j < n)
+		{
+			s[i + j] = s2[j];
+			j++;
+		}
+	}
+	else
+	{
+		while (j < s2_size)
+		{
+			s[i + j] = s2[j];
+			j++;
+		}
+	}
+	s[i + j] = "\0";
+
+	return s;
+}
