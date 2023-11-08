1c3c21b52bf5f79fc297a5da0163dbabf813fa8c
Eche <echyth@yahoo.com>
2023-11-09 09:06:55 +1000

    fix syntax errors in 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * array_range - creates an array of integers
+ * @min: minimum value in the range and the starting value in the range
+ * @max: maximum value in the range and last value in the range
+ * Return: pointer to the first element of the range or NULLL
+ * if min is greater than max
  */
 int *array_range(int min, int max)
 {
