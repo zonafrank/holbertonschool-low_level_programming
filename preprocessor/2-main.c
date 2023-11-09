4ea986da5a859727fe672ad4805f3f20358b8dab
Eche <echyth@yahoo.com>
2023-11-10 06:37:34 +1000

    create file 2-main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file containing the program
+ * Return: Always 0 (success);
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
