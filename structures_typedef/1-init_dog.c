99ae5af864fd397ec6d5d65cda16360222ed6aaf
Eche <echyth@yahoo.com>
2023-11-13 11:38:09 +1000

    fix coding style error in  1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * init_dog - creates an instance of the dog struct
+ * @d: dog struct instance
+ * @name: pointet to string: name to be assigned to the dog d
+ * @age: float: age of the dog d
+ * @owner: pointer to string: owner of the dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
