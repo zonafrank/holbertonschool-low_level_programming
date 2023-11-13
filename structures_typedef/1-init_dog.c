d0eba52920106c7bc06091ba51d4434d6947ffba
Eche <echyth@yahoo.com>
2023-11-13 11:33:37 +1000

    fix syntax error in  1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
