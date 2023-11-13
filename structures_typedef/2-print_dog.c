b92a02b68d330bde77da1e5ae09e4cda56e782bf
Eche <echyth@yahoo.com>
2023-11-14 09:20:58 +1000

    initial commit for 2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s", (d->name != NULL) ? d->name : "(nil)");
+		printf("Name: %d", d->age);
+		printf("Name: %s", (d->owner != NULL) ? d->owner : "(nil)");
+	}
+}
