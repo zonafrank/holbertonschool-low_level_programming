59ced1be9b98c88717d0bdcb19aad7eb75cd40dc
Eche <echyth@yahoo.com>
2023-11-14 09:28:30 +1000

    fix syntex error in 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,7 +6,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-		printf("Name: %f\n", d->age);
-		printf("Name: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }
