4b52a2e04db80be295c1de731408de854353f761
Eche <echyth@yahoo.com>
2023-11-13 11:03:14 +1000

    dog.h initial commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - models a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
