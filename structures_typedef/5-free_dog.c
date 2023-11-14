8801f64a5b73bf4078699bb5d90fd2129b9fac3e
Eche <echyth@yahoo.com>
2023-11-14 12:37:21 +1000

    initial commit for 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Release memory allocated to a dog structure
+ * and its components
+ * @d: a pointer to the dog structure to be freed
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
