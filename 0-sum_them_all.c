130a52ee9632974e4b3d87c7c28ee7f67bb34741
Eche <echyth@yahoo.com>
2023-11-18 09:05:47 +1000

    initial commit with variaic_functions.h and 0-sum_them_all.c

---
+++ 0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	if (n == 0)
+	{
+		va_end(ap);
+		return 0;
+	}
+
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return sum;
+}
--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,50 +1,17 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - helper function where recursive calls are made
- * 	to find the square root of the given number n
- * @low: the lower boundary value of range of number range for which
- * a middle value is to be selected
- * @high: the upper boundary value of the range of numbers for which
- * a middle value is to be selected
- * @n: the value for which a square root is to be found
- */
-int _sqrt_helper(int low, int high, int n)
-{
-	if (low <= high)
-	{
-		/* calculates middle value of the range low - high */
-		int mid = (low + high) / 2;
-		/* check if the middle value squared is equalt to n */
-		/* hence the square root */
-		if (mid * mid == n)
-		{
-			return mid;
-		}
-		/* if the square of mid is less than n and a square root */
-		/* exists, it lies in the region above mid */
-		else if (mid * mid < n)
-		{
-			return _sqrt_helper(mid + 1, high, n);
-		}
-		/* if the square of mid is greater than n and a square root */
-		/* exists, it lies in the region below mid */
-		else
-		{
-			return _sqrt_helper(low, mid - 1, n);
-		}
-	}
-	return -1;
-}
-
-/**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: the value to be raised to power y
  * @y: the power that x will be raised to
  *
  * Return: integer, value of x raised to the power of y
  */
-int _sqrt_recursion(int long n)
+int _pow_recursion(int x, int y)
 {
-	return _sqrt_helper(1, n / 2, n);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
