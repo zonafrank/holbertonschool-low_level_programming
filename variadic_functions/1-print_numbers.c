5f58cc40fb33c42be3ca58b114e435ec0b297089
Eche <echyth@yahoo.com>
2023-11-18 09:28:16 +1000

    initial commit for 1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+}
