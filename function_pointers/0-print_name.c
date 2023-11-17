72a708a3a55172a723a0771e95a30871a0c8f170
Eche <echyth@yahoo.com>
2023-11-17 21:47:55 +1000

    fix coding style errors

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,9 @@
+/**
+ * print_name - prints a name based on a call back function
+ * @name: pointer to the name string literal
+ * @f: pointer to the function for printing out the name
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,2 +1,5 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 void _putchar(int);
 void print_name(char *name, void (*f)(char *));
+#endif
