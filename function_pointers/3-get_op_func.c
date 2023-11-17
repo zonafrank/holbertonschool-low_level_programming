c61132f64b9f2f82bc9be508d604b2f84f7c7274
Eche <echyth@yahoo.com>
2023-11-17 23:59:16 +1000

    fix failing conditions in 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,10 +10,12 @@ int (*get_op_func(char *s))(int a, int b)
 	    {"/", op_div},
 	    {"%", op_mod},
 	    {NULL, NULL}};
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s; i++)
-		;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
 
 	return (ops[i].f);
 }
