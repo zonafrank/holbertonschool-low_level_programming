3fca005c290997bf00ea33007587858c5fa1e521
Eche <echyth@yahoo.com>
2023-11-17 23:50:54 +1000

    initial commit for 3-calc.h 3-op_functions.c 3-get_op_func.c 3-main.c

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+	    {"+", op_add},
+	    {"-", op_sub},
+	    {"*", op_mul},
+	    {"/", op_div},
+	    {"%", op_mod},
+	    {NULL, NULL}};
+	int i;
+
+	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s; i++)
+		;
+
+	return (ops[i].f);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "3-calc.h"
+#define EXPECTED_ARGS 4
+
+int main(int argc, char *argv[])
+{
+	int v1, v2, res;
+	char *operation;
+
+	if (argc != EXPECTED_ARGS)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	v1 = atoi(argv[1]);
+	operation = argv[2];
+	v2 = atoi(argv[3]);
+
+	if (get_op_func(operation) == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*operation == '/' || *operation == '%') && v2 == 0)
+	{
+		print("Error\n");
+		return (100);
+	}
+
+	res = get_op_func(operation)(v1, v2);
+	printf("%i", res);
+	return (0);
+}
