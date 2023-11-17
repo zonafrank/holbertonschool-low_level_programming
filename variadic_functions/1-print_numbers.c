71a30a2c158c45c8321b8b7e7cdda0bf98b8fabd
Eche <echyth@yahoo.com>
2023-11-18 09:29:48 +1000

    print new line at the end of the function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,6 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
+
+		printf("\n");
 	}
 	va_end(ap);
 }
