6553162b346ee5c8cb5e2f297655b889658d078f
Eche <echyth@yahoo.com>
2023-11-17 22:02:46 +1000

    update function_pointers.h with prototype for array_iterator

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
+#include <stdlib.h>
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 void _putchar(int);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
