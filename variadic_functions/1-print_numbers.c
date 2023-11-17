1a9272762145e0c56dcccce8ad90b2c6dad17a4a
Eche <echyth@yahoo.com>
2023-11-18 09:34:26 +1000

    fix coding style errors for 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - prints numbers, separated by an uptional
+ * separator characterfollowed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ * @...: a variable length of parameters representing n integers
+ * Return: void
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
@@ -14,8 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
-
-		printf("\n");
 	}
+	printf("\n");
 	va_end(ap);
 }
