ef301c1e2533564db172379e0879b32742c521ee
Eche <echyth@yahoo.com>
2023-11-28 20:45:39 +1000

    fix: update files to fix betty errors

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
-#include <stddef.h>
-
 #ifndef _LIST_H_
 #define _LIST_H_
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
