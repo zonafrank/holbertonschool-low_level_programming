aa06fbbcd7e45391648879361b17856f2d6e9c57
Eche <echyth@yahoo.com>
2023-11-29 21:18:00 +1000

    add function add_dnodeint in file 2-add_dnodeint.c

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list.
+ *
+ * @head: A pointer to the pointer to the head of the doubly linked list.
+ * @n: The data to be stored in the new node.
+ *
+ * Return: A pointer to the newly added node, or NULL on failure.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		printf("Error, could not malloc!");
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	*head = new;
+	return *head;
+}
