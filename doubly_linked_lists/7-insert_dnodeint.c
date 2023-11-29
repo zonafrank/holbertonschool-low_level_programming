32acdee9726ad3881e9ed664067d53a91a1f82ea
Eche <echyth@yahoo.com>
2023-11-30 09:31:04 +1000

    refactor: change logic for insert_dnodeint_at_index for failing test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,49 +3,57 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a
- * doubly linked list.
- *
- * @h: A pointer to the pointer to the head of the doubly linked list.
- * @idx: The index at which the new node should be inserted.
- * @n: The data to be stored in the new node.
- *
- * Return: A pointer to the newly inserted node, or NULL on failure.
- */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	dlistint_t *node = *h;
+	dlistint_t *head = *h;
 	dlistint_t *new;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return NULL;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return NULL;
+
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		new->next = head;
+		head->prev = new;
+		return new;
 	}
-
-	count = 1;
-	while (node)
+	else
 	{
-		if (count == idx)
+		while (head && count != idx)
 		{
-			new = malloc(sizeof(dlistint_t));
-			if (new == NULL)
-			{
-				printf("Memory allocation failed\n");
-				return (NULL);
-			}
-
-			new->n = n;
-			new->next = node->next;
-			new->prev = node;
-			node->next->prev = new;
-			node->next = new;
-
-			return (new);
+			head = head->next;
+			count++;
 		}
-		node = node->next;
-		count++;
+
+		if (head == NULL)
+		{
+			return NULL;
+		}
+
+		if (head->next == NULL)
+		{
+			head->next = new;
+			new->prev = head;
+			return new;
+		}
+		else
+		{
+			new->next = head;
+			new->prev = head->prev;
+			head->prev = new;
+			new->prev->next = new;
+			return new;
+		}
 	}
 
 	return (NULL);
