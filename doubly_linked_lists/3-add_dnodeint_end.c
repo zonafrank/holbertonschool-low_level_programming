943a2452da9e43af800a974de353562b879997ec
Eche <echyth@yahoo.com>
2023-11-29 22:08:12 +1000

    fix: coding style errors

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,6 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *h = *head;
 	dlistint_t *new = malloc(sizeof(dlistint_t));
+
 	if (!new)
 	{
 		printf("malloc allocation failed!\n");
