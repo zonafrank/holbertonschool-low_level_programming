b6c6d0b4a91a8cd1ad9feeebf637593dbd209475
Eche <echyth@yahoo.com>
2023-11-29 21:22:46 +1000

    fix: modify code logic in  function add_dnodeint

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *h = *head;
 	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
@@ -22,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->n = n;
-	new->next = *head;
+	new->next = h;
 	new->prev = NULL;
 
 	*head = new;
