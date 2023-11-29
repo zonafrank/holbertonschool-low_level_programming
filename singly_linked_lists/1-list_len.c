0ceffe6a433245103a952fcc33c63c90dba7b819
Eche <echyth@yahoo.com>
2023-11-29 10:23:13 +1000

    fix: betty style errors

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stddef.h>
 
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to linked list
+ * Return: Number of elements in the linked list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
