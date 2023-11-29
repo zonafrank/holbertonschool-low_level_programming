7aa12e11eb9246d16c0e965a7508aca4eef75223
Eche <echyth@yahoo.com>
2023-11-29 23:30:39 +1000

    fix logic error in insert_dnodeint_at_index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,11 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *node = *h;
 	dlistint_t *new;
 
-	if (idx == 0)
-	{
-		return (add_dnodeint(h, n));
-	}
-
 	while (node)
 	{
 		if (count == idx)
