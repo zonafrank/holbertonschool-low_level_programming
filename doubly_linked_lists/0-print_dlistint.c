0149e6e792069c3d3283c0b4d8a11dfea5aafee7
Eche <echyth@yahoo.com>
2023-11-29 20:39:33 +1000

    change folder name andmove files to new folder

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a doubly linked list.
+ *
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
