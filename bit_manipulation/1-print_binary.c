4a6a2de092c67cda3b0b7ce4264ce423baf5c87c
Eche <echyth@yahoo.com>
2023-12-01 12:54:00 +1000

    add function definition for print_binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number for which binary representation is to be printed
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+
+	_putchar((n % 2) + '0');
+}
