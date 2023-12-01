22a5702aa09aa85d8b2723eeb4fb4308afa6f329
Eche <echyth@yahoo.com>
2023-12-02 07:32:11 +1000

    refactor check alternate logic for flip_bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,13 +7,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m;
+	unsigned long int n_xor_m = n ^ m;
 	unsigned int bit_count = 0;
 
-	while (xor_result)
+	while (n_xor_m)
 	{
-		bit_count += xor_result & 1;
-		xor_result >>= 1;
+		bit_count += n_xor_m & 1;
+		n_xor_m >>= 1;
 	}
 
 	return (bit_count);
