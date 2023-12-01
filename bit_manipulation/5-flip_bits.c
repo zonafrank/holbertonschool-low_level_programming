1f11c6fb84105ae999b550050d5f05ee3479dfa6
Eche <echyth@yahoo.com>
2023-12-01 14:57:59 +1000

    fix coding style errors in flip_bits function

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,5 +1,6 @@
 /**
- * flip_bits - returns the number of bits needed to flip to get from one number to another
+ * flip_bits - returns the number of bits needed to flip
+ * to get from one number to another
  * @n: the first number
  * @m: the second number
  * Return: the number of bits to flip
@@ -7,13 +8,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
-	unsigned int count = 0;
+	unsigned int bit_count = 0;
 
 	while (xor_result)
 	{
-		count += xor_result & 1;
+		bit_count += xor_result & 1;
 		xor_result >>= 1;
 	}
 
-	return count;
+	return (bit_count);
 }
