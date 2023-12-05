fa395be924c4dcde55b50cc1f127c0bd83e762d8
Eche <echyth@yahoo.com>
2023-12-06 00:25:57 +1000

    fix code logic in create_file for failing test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
