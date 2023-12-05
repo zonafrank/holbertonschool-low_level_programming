ed7545ac98de36360cf79a161b355094f8261efb
Eche <echyth@yahoo.com>
2023-12-05 23:08:02 +1000

    fix code logic for failing test

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,7 +17,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	size_t letters_read;
-	/*size_t letters_written;*/
+	size_t letters_written;
 
 	int fd;
 	char *c;
@@ -26,8 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return 0;
 
 	c = malloc(sizeof(char) * letters + 1);
+
 	if (c == NULL)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -38,12 +39,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	letters_read = read(fd, c, letters);
 	c[letters_read] = '\0';
 
-	write(1, c, letters_read);
+	letters_written = write(STDOUT_FILENO, c, letters_read);
 
 	free(c);
 	close(fd);
 
-	/*if (letters_read != letters_written)
-		return 0;*/
+	if (letters_read != letters_written)
+		return 0;
+
 	return letters_read;
 }
