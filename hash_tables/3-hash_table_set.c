926e58d40c7a1c483de445f92641a6e3a8583536
Eche <echyth@yahoo.com>
2023-12-07 00:16:40 +1000

    test alternate code logic

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,10 +14,9 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new, *curr_node;
 	char *value_copy;
 	unsigned long int index;
-	unsigned long int i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
@@ -26,14 +25,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_copy == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	curr_node = ht->array[index];
+	while (curr_node != NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(curr_node->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(curr_node->value);
+			curr_node->value = value_copy;
 			return (1);
 		}
+		curr_node = curr_node->next;
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
