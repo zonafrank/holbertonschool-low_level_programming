3aaff1e014f6b1936e03298121183ed42559f475
Eche <echyth@yahoo.com>
2023-12-07 11:47:45 +1000

    add implementation for function hash_table_delete

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (node != NULL)
+		{
+			while (node)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
