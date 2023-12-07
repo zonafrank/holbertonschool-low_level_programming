2415a5c69936db9d8977f42056187e72462d167a
Eche <echyth@yahoo.com>
2023-12-07 12:21:55 +1000

    fix: add documentation for hash_table_delete

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - Deletes a hash table and its elements.
+ *
+ * @ht: A pointer to the hash table to be deleted.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
