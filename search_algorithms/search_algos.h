4945d3d1258b73d6f04edff701423044a4a05417
Eche <echyth@yahoo.com>
2024-01-18 23:14:38 +1000

    initial commit

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
